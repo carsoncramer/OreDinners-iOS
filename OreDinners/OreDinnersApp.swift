c34c51868f3c7ad0af65d61434ec91bfb75b69d6
CarsonFractyl <carsonfractyldev@gmail.com>
2023-03-08 14:20:48 -0700

    Initial Commit

---
+++ OreDinners/OreDinnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OreDinnersApp.swift
+//  OreDinners
+//
+//  Created by Carson Cramer on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct OreDinnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
